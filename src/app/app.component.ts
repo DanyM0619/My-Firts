70e0bd8b214f8922251288ac07cfd15a2a5f647d
Dany <daedmegu@gmail.com>
2021-09-07 08:30:44 -0500

    Calulador - eventos - relacionar padre hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'first';
+  public title: string = 'Primer Programa de Angular';
+  numero1: number;
+  numero2: number;
+  resultado: number;
+  /**var:  
+   * let: son las que se quedan dentro de los metodos y si salen se destruyen
+   * const: son las que utilizan un valor constante y no cambian
+   * this: Se utiliza esta palabra de la forma global, si se quita surge un error
+   **/
+
+  public operar(op: string): void{
+    switch(op){
+      case 's': 
+        this.resultado = this.numero1 + this.numero2;
+        break;
+      case 'r':
+        this.resultado = this.numero1 - this.numero2;
+        break;
+      case 'm':
+        this.resultado = this.numero1 * this.numero2;
+        break;
+      case 'd':
+        this.resultado = this.numero1 / this.numero2;
+        break;
+      default:
+        this.resultado = 0;
+    }
+  }
+
+public teclear(e: any): void{
+  console.log(e);
+  console.log("La tecla que se oprimio fue: "+e.key);
+  console.log("La palabra completa es: "+e.target.value);
+}
 }
